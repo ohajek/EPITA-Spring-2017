7083c3dc6a8697e2d625eb501706e84a3e4cecad
Guest User <Guest@R1P1-47.local>
2017-05-10 19:20:18 +0200

    merge diarhoea II

---
+++ Advanced iOS Spring 2017/Advanced iOS Spring 2017/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Advanced iOS Spring 2017
+//
+//  Created by Suiz Uzcategui on 19/04/2017.
+//  Copyright © 2017 ___AdvancediOS___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let weather = WeatherFetch()
+        weather.getWeather(city: "Berlin")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
